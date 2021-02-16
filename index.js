e4b0c951f0036f62fcf060238779ce49c70a44e0
Ariel <arielpiccolo@gmail.com>
2021-02-16 01:12:52 +0000

    bug fixed -update

--- index.js
+++ index.js
@@ -48,37 +48,9 @@ app.get("/", (req, res) => {
 
 });
 
-// set a search by category page app
-app.get("/search", (req, res) => {
-    res.render("search");
-});    
-
-
-//  set a display category page app doing a query to the 'theCategory' input
-app.get("/displayCategory", (req, res) => {
-    // next we use query instead of body because we are graving values from the URL
-    const category = req.query.theCategory;
-
-
-    // set a new variable 'jokeURL' path to the api URL for categories and pass our category variable
-    const jokeURL = `https://api.chucknorris.io/jokes/random?category=${category}`
-
-    request( { url: jokeURL, json: true }, (error, response) => {
-        if (response.body.error) {
-            res.render('index',  {
-                joke: "Sorry that category does not exist"
-            })
-        } else {  
-            res.render('index', {
-                joke: response.body.value
-            })
-       }
-    });   
-    
-});
 
 
 // server port
 app.listen(5000, () => {
-    console.log("server running @port 4000")
+    console.log("server running @port 5000")
 });
